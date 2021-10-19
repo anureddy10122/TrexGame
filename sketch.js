c5582e1c108f16a17dad9c5d1ba6ad2ab7bc27cc
anushaprakash1012 <76881960+anushaprakash1012@users.noreply.github.com>
2021-10-19 19:38:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+var trex ,trex_running;
+function preload(){
+  
+trex_running = loadAnimation("trex1.png","trex3.png","trex4.png")
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+
+  trex = createSprite(100,180,50,50);
+  trex.addAnimation("running",trex_running);
+  trex.scale = 0.5;
+
+  edges = createEdgeSprites();
+ 
+}
+
+function draw(){
+  background("lightblue")
+
+  if(keyDown("space")){
+    
+    trex.velocityY = -8;
+
+  }
+
+  trex.velocityY = trex.velocityY + 0.8; // gravity
+
+  trex.collide(edges);
+
+  
+
+  drawSprites();
+
+}
